1b19f518bbfd0fa2e96b49c3458acc9c4146b442
Shibilyk <shibilyk11@gmail.com>
2024-12-20 14:11:16 +0530

    update cart

--- src/conponents/Header/Header.js
+++ src/conponents/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./Header.css"
+import { useSelector } from 'react-redux'
 const Header = () => {
+  const {cartList = []}  = useSelector((state)=>state.cart)
+  const totelCount = cartList.reduce((acc,value)=>acc+=value.count,0)
   return (
    <header>
     <div className="container">
         <h1>HelloWorld</h1>
         <div className='right-section'>
-          <div className="count">0</div>
+          <div className="count">{totelCount}</div>
         <img src="/image.png" alt="" />
         </div>
     </div>
