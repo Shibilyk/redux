0579fe460999cd009a1346aec11fb22c7f61894e
Shibilyk <shibilyk11@gmail.com>
2024-12-31 15:52:15 +0530

    add before cart

--- src/conponents/Header/CartButtons/beforeCart.js
+++ src/conponents/Header/CartButtons/beforeCart.js
@@ -2,15 +2,18 @@ import React from "react";
 import { addToCart } from "../../../redux/cart";
 import "./cart-button.css";
 import { useDispatch } from "react-redux";
-const BeforeCart  = ({product}) => {
+const BeforeCart = ({ product }) => {
   const dispatch = useDispatch();
   return (
     <div className="before-cart">
-      <button className="add-cart-button" onClick={() => dispatch(addToCart(product))}>
+      <button
+        className="add-cart-button"
+        onClick={() => dispatch(addToCart(product))}
+      >
         Add to cart
       </button>
     </div>
   );
 };
 
-export default BeforeCart ;
+export default BeforeCart;
